65fd50f03da1608690df4302713203d916c93322
ArturFortunato <artur.fortunato1@gmail.com>
2020-09-02 21:00:22 +0100

    Connected server to frontend

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,25 @@
 import flask
+import flask_cors   
 import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+flask_cors.CORS(app)
 
 
 @app.route('/schedule', methods=['GET'])
 def home():
     #Should receive a string with the courses comma separated
     print("This", flask.request.args.get('courses'))
-    x = {'a': 3, 'b': 5}
-    return x
+    x = {0: [
+                {
+                    'name': 'test',
+                    'start': '2020-09-04 10:00:00',
+                    'end': '2020-09-04 11:30:00',
+                    'color': 'red',
+                },
+            ]
+    }
+    return json.dumps(x)
 
 app.run()
